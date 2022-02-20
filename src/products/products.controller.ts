ccc051a720738e033cab5e498b4f7986b6ec99cd
sahildotexe <sahilkaling@gmail.com>
2022-02-20 16:42:37 +0530

    CRUD done

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,4 +1,12 @@
-import { Controller, Post, Body, Get } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  Get,
+  Param,
+  Patch,
+  Delete,
+} from '@nestjs/common';
 import { ProductsService } from './products.service';
 
 @Controller('products')
@@ -20,7 +28,29 @@ export class ProductsController {
   }
 
   @Get()
-  getAllProducts(): any {
+  getAllProducts() {
     return this.productsService.getProducts();
+  }
+
+  @Get(':id')
+  getProduct(@Param('id') prodId: string) {
+    return this.productsService.getProduct(prodId);
+  }
+
+  @Patch(':id')
+  updateProduct(
+    @Param('id') prodId: string,
+    @Body('title') prodTitle: string,
+    @Body('description') prodDesc: string,
+    @Body('price') prodPrice: number,
+  ) {
+    this.productsService.updateProduct(prodId, prodTitle, prodDesc, prodPrice);
+    return null;
+  }
+
+  @Delete(':id')
+  deleteProduct(@Param('id') prodId: string) {
+    this.productsService.deleteProduct(prodId);
+    return null;
   }
 }
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,18 +1,54 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Product } from './product.model';
 
 @Injectable()
 export class ProductsService {
-  products: Product[] = [];
+  private products: Product[] = [];
 
   insertProduct(title: string, desc: string, price: number) {
-    const prodId = new Date().toString();
+    const prodId = Math.random().toString();
     const newProduct = new Product(prodId, title, desc, price);
     this.products.push(newProduct);
     return prodId;
   }
 
   getProducts() {
-    return this.products;
+    return [...this.products];
+  }
+
+  getProduct(productId: string) {
+    const product = this.findProduct(productId[0]);
+    return { ...product };
+  }
+
+  updateProduct(productId: string, title: string, desc: string, price: number) {
+    const [product, index] = this.findProduct(productId);
+    const updatedProduct = { ...product };
+    if (title) {
+      updatedProduct.title = title;
+    }
+    if (desc) {
+      updatedProduct.desc = desc;
+    }
+    if (price) {
+      updatedProduct.price = price;
+    }
+    this.products[index] = updatedProduct;
+  }
+
+  deleteProduct(productId: string) {
+    const [product, index] = this.findProduct(productId);
+    this.products.splice(index, 1);
+  }
+
+  private findProduct(productId): [Product, number] {
+    const productIndex = this.products.findIndex(
+      (prod) => prod.id === productId,
+    );
+    const product = this.products[productIndex];
+    if (!product) {
+      throw new NotFoundException('Could not find product.');
+    }
+    return [product, productIndex];
   }
 }
